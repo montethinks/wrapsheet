4d7057818372eb43e3a2cd615f1816a94bcb4203
Monte Williams <monte.williams@gmail.com>
2019-07-19 11:48:57 -0400

    bootstrap config

--- lib/wrapsheet/configuration.rb
+++ lib/wrapsheet/configuration.rb
@@ -1,5 +1,15 @@
 module Wrapsheet
   class Configuration
+    attr_accessor :google_account_type
+
+    class << self
+      attr_accessor :config
+    end
+
+    def self.configure(&block)
+      self.config = new
+      block[config]
+    end
 
   end
 end
--- spec/wrapsheet/configuration_spec.rb
+++ spec/wrapsheet/configuration_spec.rb
@@ -1,6 +1,26 @@
 RSpec.describe Wrapsheet::Configuration do
+  after :each do
+    described_class.instance_variable_set(:@config, nil)
+  end
   describe 'in ruby' do
-    it 'has google_account_type'
+    it 'is configurable' do
+      described_class.configure do |config|
+        expect(config).to be_a described_class
+      end
+    end
+
+    it 'stores a singleton of the configuration' do
+      described_class.configure { }
+      expect(described_class.config).to be_a described_class
+    end
+
+    it 'has google_account_type' do
+      described_class.configure do |config|
+        config.google_account_type = 'avalue'
+      end
+
+      expect(described_class.config.google_account_type).to eq('avalue')
+    end
   end
   describe 'from ENV'
 end
